1d9347bf48c4cc94eaefe0aa1a79acb3b6af97dc
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-09-05 23:38:42 +0700

    tambah 1

---
+++ ContohVariabelNo13.java
@@ -0,0 +1,15 @@
+public class ContohVariabelNo13 {
+    public static void main(String[] args) {
+        String salahsatuHobysayaAdalah = "bermain petak umpet";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte _umurSayaSekarang = 20;
+        double $ipk = 3.24, tinggi = 1.78;
+        System.out.println("hoby? " + salahsatuHobysayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+        
+    }
+}
