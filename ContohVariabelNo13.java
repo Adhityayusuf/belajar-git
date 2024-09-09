e7f07351157c3f13f25107208013cb27b51eedb8
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-09-09 20:05:46 +0700

    p

--- ContohVariabelNo13.java
+++ ContohVariabelNo13.java
@@ -5,8 +5,8 @@ public class ContohVariabelNo13 {
         char jenisKelamin = 'L';
         byte _umurSayaSekarang = 20;
         double $ipk = 3.24, tinggi = 1.78;
-        System.out.println("hoby? " + salahsatuHobysayaAdalah);
-        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("hoby saya : " + salahsatuHobysayaAdalah);
+        System.out.println("Apakah pandai: " + isPandai);
         System.out.println("Jenis kelamin: " + jenisKelamin);
         System.out.println("Umurku saat ini: " + _umurSayaSekarang);
         System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
