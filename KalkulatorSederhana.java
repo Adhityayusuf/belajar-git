c050884482f9f82d5f03c3973cb2c5d7af76f960
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-09-09 20:28:37 +0700

    ubah

--- KalkulatorSederhana.java
+++ KalkulatorSederhana.java
@@ -2,22 +2,21 @@ public class KalkulatorSederhana {
     public static void main(String[] args) {
         byte x = 5;
         byte y = 7;
-        
+
         int hasil_1;
         int hasil_2;
         int hasil_3;
         int hasil_4;
 
-        hasil_1 = x+y;
-        hasil_2 = x-y;
-        hasil_3 = x*y;
-        hasil_4 = x%y;
+        hasil_1 = x + y;
+        hasil_2 = x - y;
+        hasil_3 = x * y;
+        hasil_4 = x % y;
 
         System.out.println(hasil_1);
         System.out.println(hasil_2);
         System.out.println(hasil_3);
         System.out.println(hasil_4);
-            
-        
+
     }
 }
