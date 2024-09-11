cb7c03a3f78f04f418c0336153ca3e5e49c52995
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-09-11 20:03:52 +0700

    tambah

---
+++ ContohTipeData.java
@@ -0,0 +1,19 @@
+public class ContohTipeData {
+    public static void main(String[] args) {
+        char golonganDarah = 'B';
+        byte jarak = (byte) 130;
+        short jumlahPendudukDalamsatuDusun = 1025;
+        float Suhu = 60.50F;
+        double berat = 0.5467812345;
+        long saldo = 150000000;
+        int angkaDesimal = 0x10;
+        
+        System.out.println("Golongan darah\t\t\t\t\t\t: " + golonganDarah);
+        System.out.println("Jarak\t\t\t\t\t\t\t\t: " + jarak);
+        System.out.println("Jumlah penduduk dalam satu dusun\t: " + jumlahPendudukDalamsatuDusun);
+        System.out.println("Suhu\t\t\t\t\t\t\t\t: " + Suhu);
+        System.out.println("Berat\t\t\t\t\t\t\t\t: " + berat);
+        System.out.println("saldo\t\t\t\t\t\t\t\t: " + saldo);
+        System.out.println("Angka Desimal\t\t\t\t\t\t: " + angkaDesimal);
+    }
+}
