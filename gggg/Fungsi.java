40705569814188a75f0bd30d2fb976ebc1d53e89
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-01 20:37:50 +0700

    p

---
+++ gggg/Fungsi.java
@@ -0,0 +1,16 @@
+public class Fungsi {
+    public static void main(String[] args) {
+ 
+         Kalkulator(5, 4);
+ 
+    }
+    
+    public static void Kalkulator (int a, int b) {
+ 
+     System.out.println(a + b);
+     System.out.println(a - b);
+     System.out.println(a * b);
+     System.out.println(a / b);
+    }
+ }
+     
