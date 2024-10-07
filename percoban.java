b59473941d716c3dd5372f121fe93950bdc0bbd9
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-07 23:01:00 +0700

    P

---
+++ percoban.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class percoban {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Kelipatan, Jumlah = 0, Counter = 0, total = 0; 
+
+        System.out.print("Masukkan bilangan Kelipatan (1-9): ");
+        Kelipatan = sc.nextInt();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % Kelipatan == 0) {
+                total += i;
+                Counter++;
+            }
+        
+            System.out.println("Banyak bilangan " + Kelipatan + " dari 1 sampai 50 adalah " + Counter);
+            break;
+        }
+    }
+}
